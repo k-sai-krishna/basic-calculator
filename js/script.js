cd2b356bb9a683cec9a60fd1a7c705d8751df5e1
Kotha Sai Krishna <kotha.saikrishna07@gmail.com>
2025-01-19 14:16:22 +0530

    refactor: commented the testing lines

--- js/script.js
+++ js/script.js
@@ -2,13 +2,18 @@ let expression = '';
 let error_message = document.getElementById('error-here');
 let btns = document.querySelectorAll('.button');
 let input = document.querySelector('input');
+// console.log(btns);
+// for (let index = 0; index < btns.length; index++) {
+//   const element = btns[index];
+//   console.log(element.innerHTML);
+// }
 let buttons = Array.from(btns);
 buttons.forEach(button => {
   button.addEventListener('click', e => {
     if (e.target.innerHTML === 'AC') {
       input.value = '';
       expression = '';
-      error_message.className = 'disable'
+      error_message.className = 'disable';
     } else if (e.target.innerHTML === 'X') {
       let new_expression = '';
       for (let i = 0; i < expression.length - 1; i++) {
@@ -21,18 +26,20 @@ buttons.forEach(button => {
       try {
         expression = eval(expression);
         input.value = expression;
+        console.log('Expression Evaluated :)');
       } catch (error) {
         // alert('Invalid input');
         error_message.className = 'red';
         error_message.innerHTML = 'Invalid input boss!!';
-        console.log(error_message.className);
+        // console.log(error_message.className);
       }
     } else {
       expression += e.target.innerHTML;
       input.value = expression;
       error_message.className = 'disable';
-      console.log(error_message.className);
+      // console.log(error_message.className);
     }
-    console.log(e.target);
+    // console.log(e.target.innerHTML);
+    // console.log(e);
   });
 });
